4ca5658cb0b2b8b93959fe2c692e973baa095044
imustafayasin <imustafayasin@gmail.com>
2023-10-15 19:48:06 +0300

    Add shade vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="max-w-7xl m-auto">
     <slot />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,27 @@
 <template>
-    
+  <div class="h-screen flex items-center justify-center">
+    <div class="border w-full max-w-md p-4 mx-auto">
+      <UiRadioGroup default-value="option-one" class="flex mb-8 justify-center">
+        <div class="flex items-center space-x-2">
+          <UiRadioGroupItem id="option-one" value="option-one" />
+          <UiLabel for="option-one">Bus</UiLabel>
+        </div>
+        <div class="flex items-center space-x-2">
+          <UiRadioGroupItem id="option-two" value="option-two" />
+          <UiLabel for="option-two">Flight</UiLabel>
+        </div>
+      </UiRadioGroup>
+      <UiButton>Click me</UiButton>
+      <UiInput type="email" placeholder="Email" />
+    </div>
+  </div>
 </template>
+
+<script setup>
+const supabase = useSupabaseClient();
+const cities = ref();
+(async () => {
+  const { data } = await supabase.from('City').select();
+  cities.value = data
+})()
+</script>
