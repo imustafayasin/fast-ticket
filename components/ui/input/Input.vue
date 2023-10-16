9c612b82dd58cc7ee416a2227b99626be00b5429
imustafayasin <imustafayasin@gmail.com>
2023-10-16 21:18:18 +0300

    Add datepicker and generic shadcn-vue components

--- components/ui/input/Input.vue
+++ components/ui/input/Input.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { useVModel } from '@vueuse/core'
 import { cn } from '@/lib/utils'
+import { Label } from 'radix-vue'
 
 const props = defineProps<{
   defaultValue?: string | number
+  label?: string | number
   modelValue?: string | number
 }>()
 
@@ -18,5 +20,12 @@ const modelValue = useVModel(props, 'modelValue', emits, {
 </script>
 
 <template>
+   <Label
+    v-if="label"
+    v-bind="props"
+    :class="cn('block text-sm tracking-tight font-medium text-foreground text-left')"
+  >
+    {{ label }}
+  </Label>
   <input v-model="modelValue" type="text" :class="cn('flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50', $attrs.class ?? '')">
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="h-screen flex items-center justify-center">
-    <div class="border w-full max-w-md p-4 mx-auto">
-      <UiRadioGroup default-value="option-one" class="flex mb-8 justify-center">
+    <div class="border w-full max-w-sm p-4 mx-auto rounded">
+      <UiRadioGroup default-value="option-one" class="flex mb-4 justify-center">
         <div class="flex items-center space-x-2">
           <UiRadioGroupItem id="option-one" value="option-one" />
           <UiLabel for="option-one">Bus</UiLabel>
@@ -11,17 +11,26 @@
           <UiLabel for="option-two">Flight</UiLabel>
         </div>
       </UiRadioGroup>
-      <UiButton>Click me</UiButton>
-      <UiInput type="email" placeholder="Email" />
+      <UiSelect label="Depart city" class="mb-3" placeholder="Select city"
+        :data="cities.map(t => ({ value: t.id, name: t.name }))">Find ticket</UiSelect>
+      <UiSelect label="Destination city" class="mb-3" placeholder="Select city"
+        :data="cities.map(t => ({ value: t.id, name: t.name }))">Find ticket</UiSelect>
+      <UiDatePicker label="Date" v-model="date" class="w-full mb-4" />
+      <UiButton class="block ml-auto">Find ticket</UiButton>
     </div>
   </div>
 </template>
 
 <script setup>
 const supabase = useSupabaseClient();
-const cities = ref();
-(async () => {
-  const { data } = await supabase.from('City').select();
+
+const cities = ref([]);
+const date = ref(new Date())
+
+const fetchCities = async () => {
+  const { data } = await supabase.from('City').select().in('id', [435, 436, 407, 1, 417]);
   cities.value = data
-})()
+};
+
+fetchCities();
 </script>
